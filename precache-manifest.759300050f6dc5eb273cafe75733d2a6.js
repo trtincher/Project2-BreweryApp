6d636530e0d36eeaff96f28bbc83538a4985dc9d
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-07-01 17:18:53 -0500

    Updates

---
+++ precache-manifest.759300050f6dc5eb273cafe75733d2a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16783b26919e5582a376b2b70a27a22b",
+    "url": "/Project2-BreweryApp/index.html"
+  },
+  {
+    "revision": "527637627d654c75d5ab",
+    "url": "/Project2-BreweryApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "00db55f43adaff9b89c2",
+    "url": "/Project2-BreweryApp/static/css/main.6f426c4c.chunk.css"
+  },
+  {
+    "revision": "527637627d654c75d5ab",
+    "url": "/Project2-BreweryApp/static/js/2.7bedbfe9.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/Project2-BreweryApp/static/js/2.7bedbfe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00db55f43adaff9b89c2",
+    "url": "/Project2-BreweryApp/static/js/main.e06da152.chunk.js"
+  },
+  {
+    "revision": "8d94849c0a9aec62641e",
+    "url": "/Project2-BreweryApp/static/js/runtime-main.73f298bd.js"
+  }
+]);
